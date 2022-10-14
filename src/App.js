4553bac4fa1e4602e264ed0031dcaa9ebf1fbf7f
Francis Pelletier <francispelletier2@hotmail.com>
2022-10-14 15:00:10 -0400

    When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleText(e) {
+    e.preventDefault();
+    setText(e.target.value);
+  }
+
   return (
     <main className="main">
       <header>
         <h1 className="primary-header">Markdown Previewer</h1>
       </header>
+      <textarea id="editor" onChange={handleText} />
+      <section id="preview">{text}</section>
     </main>
   );
 }
